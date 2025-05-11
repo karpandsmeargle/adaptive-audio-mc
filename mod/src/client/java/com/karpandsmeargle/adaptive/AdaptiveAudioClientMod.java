6f3d109670f973c6029b5d87d65f999e4db1cf05
karpandsmeargle <karpandsmeargle@gmail.com>
2025-05-10 20:24:47 -0700

    Make a logger

--- mod/src/client/java/com/karpandsmeargle/adaptive/AdaptiveAudioClientMod.java
+++ mod/src/client/java/com/karpandsmeargle/adaptive/AdaptiveAudioClientMod.java
@@ -2,9 +2,15 @@ package com.karpandsmeargle.adaptive;
 
 import net.fabricmc.api.ClientModInitializer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class AdaptiveAudioClientMod implements ClientModInitializer {
+    public static final String MOD_ID = "adaptive-audio-mc";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
     @Override
     public void onInitializeClient() {
-        // This entrypoint is suitable for setting up client-specific logic, such as rendering.
+        LOGGER.info("Client mod initialized!");
     }
 }
